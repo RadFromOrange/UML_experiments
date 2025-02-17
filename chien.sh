f5b450598335d9855f873004c2f2b0e9c8f5f918
RadFromOrange <radoinidrissi@hotmail.fr>
2025-02-17 18:48:10 +0000

    fff

--- chien.sh
+++ chien.sh
@@ -6,6 +6,7 @@ apt-get install -y fakechroot fakeroot debootstrap slirp user-mode-linux vim tre
 rm -fr ./debian-rootfs/
 mkdir ./debian-rootfs/ 
 fakechroot fakeroot debootstrap --include=net-tools,iproute2,sl  bookworm ./debian-rootfs/  http://deb.debian.org/debian/
+touch ./debian-rootfs/salop
 # sudo chroot ./debian-rootfs apt install net-tools
 
 
@@ -17,7 +18,7 @@ apt-get install fuse2fs fuse
 sed -i 's/#user_allow_other/user_allow_other/g'   /etc/fuse.conf
 fuse2fs -o allow_other,uid=$(id -u),gid=$(id -g) root_fs  ./tmp/mnt/uml
 
-cp -a debian-rootfs/. ./tmp/mnt/uml
+cp -a debian-rootfs/* ./tmp/mnt/uml
 touch ./tmp/mnt/uml/salop
 ls -alrt ./tmp/mnt/uml
 umount ./tmp/mnt/uml
